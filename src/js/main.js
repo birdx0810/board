1900f2e90d88c7b4668e12845b00d3abe46694e4
Rispolyv0n <navy.horizon.314@gmail.com>
2021-03-18 01:54:56 +0800

    Resize -> smaller.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,62 @@
+$(document).ready(function() {
+    console.log("document ready");
+
+    // initialize textarea size
+    textarea = $("textarea")
+
+    innerHeight = textarea.innerHeight();
+    innerWidth = textarea.innerWidth();
+    console.log('Inner:', innerHeight, innerWidth);
+
+    scrollHeight = textarea.prop('scrollHeight');
+    scrollWidth = textarea.prop('scrollWidth');
+    console.log('Scroll:', scrollHeight, scrollWidth)
+
+
+    // set font size
+    textarea.css('font-size', innerHeight);
+    console.log(textarea.css('font-size'));
+
+    textarea.bind('input propertychange', function() {
+        console.log("input change")
+    
+        innerHeight = textarea.innerHeight();
+        innerWidth = textarea.innerWidth();
+        console.log('Inner:', innerHeight, innerWidth);
+
+        scrollHeight = textarea.prop('scrollHeight');
+        scrollWidth = textarea.prop('scrollWidth');
+        console.log('Scroll:', scrollHeight, scrollWidth)
+
+        // if overflow
+        let err = 3;
+        let maxCount = 100;
+        let curCount = 0;
+        while (scrollHeight - innerHeight > err || scrollWidth - innerWidth > err){
+            textarea = $("textarea")
+
+            console.log('Detect overflow, resize font size.')
+            
+            curFontSize = parseInt(textarea.css('font-size'))
+            textarea.css('font-size', (curFontSize-10) + 'px');
+            console.log(textarea.css('font-size'));
+
+            innerHeight = textarea.innerHeight();
+            innerWidth = textarea.innerWidth();
+            console.log('Inner:', innerHeight, innerWidth);
+
+            scrollHeight = textarea.prop('scrollHeight');
+            scrollWidth = textarea.prop('scrollWidth');
+            console.log('Scroll:', scrollHeight, scrollWidth)
+
+            curCount += 1;
+            if (curCount > maxCount) break;
+        }
+
+  });
+});
+
+
 function toggleColor(){
     // Get background color
     bgColor = $("body").css("background-color");
