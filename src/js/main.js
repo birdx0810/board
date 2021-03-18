d26fee966c3ebea897a654f9208b0a56cb97ea2d
Rispolyv0n <navy.horizon.314@gmail.com>
2021-03-18 15:58:06 +0800

    Clean code.

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
-    console.log("document ready");
-
-    // initialize textarea size
+    // initialize font size
     textarea = $("textarea")
-
-    innerHeight = textarea.innerHeight();
-    innerWidth = textarea.innerWidth();
-    console.log('Inner:', innerHeight, innerWidth);
-
-    scrollHeight = textarea.prop('scrollHeight');
-    scrollWidth = textarea.prop('scrollWidth');
-    console.log('Scroll:', scrollHeight, scrollWidth)
-
-
-    // set font size
     textarea.css('font-size', innerHeight);
     console.log(textarea.css('font-size'));
 
+    // if input change
     textarea.bind('input propertychange', function() {
         console.log("input change")
     
@@ -28,30 +16,99 @@ $(document).ready(function() {
         scrollWidth = textarea.prop('scrollWidth');
         console.log('Scroll:', scrollHeight, scrollWidth)
 
-        // if overflow
         let err = 3;
-        let maxCount = 100;
+        let maxCount_small = 200;
+        let maxCount_big = 20;
         let curCount = 0;
-        while (scrollHeight - innerHeight > err || scrollWidth - innerWidth > err){
-            textarea = $("textarea")
+        let step_small = 10;
+        let step_big = 50;
 
-            console.log('Detect overflow, resize font size.')
+        // if overflow
+        while (scrollHeight - innerHeight > err || scrollWidth - innerWidth > err){
+            console.log('Detect overflow, font size should be smaller.')
             
             curFontSize = parseInt(textarea.css('font-size'))
-            textarea.css('font-size', (curFontSize-10) + 'px');
-            console.log(textarea.css('font-size'));
+
+            textarea.css('font-size', (curFontSize-step_small) + 'px');
 
             innerHeight = textarea.innerHeight();
             innerWidth = textarea.innerWidth();
-            console.log('Inner:', innerHeight, innerWidth);
 
             scrollHeight = textarea.prop('scrollHeight');
             scrollWidth = textarea.prop('scrollWidth');
-            console.log('Scroll:', scrollHeight, scrollWidth)
 
             curCount += 1;
-            if (curCount > maxCount) break;
+            if (curCount > maxCount_small) break;
         }
+
+        // if underflow
+        if (curCount < 50){
+            // bigger
+            while (scrollHeight - innerHeight <= err || scrollWidth - innerWidth <= err){
+                console.log('Bigger.')
+                
+                curFontSize = parseInt(textarea.css('font-size'))
+    
+                textarea.css('font-size', (curFontSize+step_big) + 'px');
+    
+                innerHeight = textarea.innerHeight();
+                innerWidth = textarea.innerWidth();
+    
+                scrollHeight = textarea.prop('scrollHeight');
+                scrollWidth = textarea.prop('scrollWidth');
+    
+                curCount += 1;
+                if (curCount > maxCount_big) break;
+            }
+
+            // then smaller
+            curCount = 0;
+            while (scrollHeight - innerHeight > err || scrollWidth - innerWidth > err){
+                console.log('Detect overflow 2, font size should be smaller.')
+                
+                curFontSize = parseInt(textarea.css('font-size'))
+    
+                textarea.css('font-size', (curFontSize-step_small) + 'px');
+    
+                innerHeight = textarea.innerHeight();
+                innerWidth = textarea.innerWidth();
+    
+                scrollHeight = textarea.prop('scrollHeight');
+                scrollWidth = textarea.prop('scrollWidth');
+    
+                curCount += 1;
+                if (curCount > maxCount_small) break;
+            }
+        }
+
+
+        // var lht = parseInt(textarea.css('lineHeight'));
+        // console.log(lht)
+        // var lines = parseInt(scrollHeight) / lht;
+        // console.log('line num:', lines);
+
+        // console.log('line num:', textarea[0].value.substr(0, textarea.selectionStart).split("\n").length);
+
+        // curCount = 0;
+        // while (innerHeight - scrollHeight > err || innerWidth - scrollWidth > err){
+        //     console.log('Detect underflow, font size should be bigger.')
+
+        //     curFontSize = parseInt(textarea.css('font-size'))
+            
+        //     textarea.css('font-size', (curFontSize+step) + 'px');
+        //     console.log(textarea.css('font-size'));
+
+        //     innerHeight = textarea.innerHeight();
+        //     innerWidth = textarea.innerWidth();
+        //     console.log('Inner:', innerHeight, innerWidth);
+
+        //     scrollHeight = textarea.prop('scrollHeight');
+        //     scrollWidth = textarea.prop('scrollWidth');
+        //     console.log('Scroll:', scrollHeight, scrollWidth)
+
+        //     curCount += 1;
+        //     if (curCount > maxCount) break;
+        // }
 
   });
 });
